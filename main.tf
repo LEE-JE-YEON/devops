0573f962324ecadfe9185fef663e41454076fb8e
desktop <dlwpdus01@gmail.com>
2022-04-16 14:34:38 +0900

    key 삭제

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
     region      = "ap-northeast-2"
-    access_key  = "AKIA52JYMXBFA4GCNAWU"
-    secret_key  = "WUu7q0B3rSnXP7YSRbSn4yz2Glps58Wyt71JWri5"
 }
 
 resource "aws_instance" "example" {
