bb1bb1f1b36b14c3af252485020f495b5825074f
desktop <dlwpdus01@gmail.com>
2022-04-16 16:23:13 +0900

    version, security_group 테라폼 파일 추가

--- main.tf
+++ main.tf
@@ -2,7 +2,18 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "test_instance" {
   ami           = "ami-0454bb2fefc7de534"
   instance_type = "t2.micro"
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_vpc" "main_VPC" {
+  cidr_block = "172.29.0.0/16"
+  instance_tenancy = "default"
+  tags = {
+    Name = "Main VPC"
+  }
 }
---
+++ security_group.tf
@@ -0,0 +1,31 @@
+resource "aws_default_security_group" "default_SG" {
+  vpc_id      = "${aws_vpc.main_VPC.id}"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "port 22 allow"
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "port 80 allow"
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "port 443 allow"
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "All allow"
+  }
+}
