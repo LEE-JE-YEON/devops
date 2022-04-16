e90fb67358c0e854239991f22722c181d9d9b93c
desktop <dlwpdus01@gmail.com>
2022-04-16 17:01:40 +0900

    description 수정

--- security_group.tf
+++ security_group.tf
@@ -5,27 +5,27 @@ resource "aws_default_security_group" "default_SG" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-    description = "22 포트 허용"
+    description = "allow port 22"
   }
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-    description = "80 포트 허용"
+    description = "allow port 80"
   }
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-    description = "443 포트 허용"
+    description = "allow port 443"
   }
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-    description = "전부 허용"
+    description = "allow all"
   }
 }
